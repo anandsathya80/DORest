34ddc9a426f02219b4ae56c9812910ce1dcc56b5
anandsathya80 <anandsathya80@gmail.com>
2024-08-06 10:53:43 +0700

    feat: routing api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\food\FoodController;
+use App\Http\Controllers\food\FoodTypeController;
+use App\Http\Controllers\order\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// food type
+Route::get('/foodType', [FoodTypeController::class, 'index']);
+Route::post('/foodType', [FoodTypeController::class, 'store']);
+
+// foods
+Route::get('/foods', [FoodController::class, 'index']);
+Route::post('/foods', [FoodController::class, 'store']);
+
+// order
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
