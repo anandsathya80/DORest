1e7da7dc5bbbcbd029549a64d0cf17333d9214ac
anandsathya80 <anandsathya80@gmail.com>
2024-08-06 10:54:56 +0700

    feat: order controller

---
+++ app/Http/Controllers/order/OrderController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\order;
+
+use App\Http\Controllers\Controller;
+use App\Models\OrderDetail;
+use App\Models\Orders;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $order = Orders::all();
+        $formattedorder = $order->map(function ($order) {
+            return $this->formatOrder($order);
+        });
+
+        return response()->json(['order' => $formattedorder,]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $order = Orders::create([
+            'user_id' => $request->input('user_id'),
+            'customer_name' => $request->input('customer_name'),
+        ]);
+        // $detailOrder = OrderDetail::create([
+        //     'order_id' => $order->id,
+        //     'food_id' => $request->input('food_id'),
+        //     'food_qty' => $request->input('food_qty'),
+        // ]);
+
+        return response()->json([$order, 'created successfully']);
+        // return response()->json([$detailOrder, 'detail created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $detailOrder = OrderDetail::find($id);
+        $formattedDetailOrder = $detailOrder->map(function ($detailOrder) {
+            return $this->formatCustomer($detailOrder);
+        });
+
+        return response()->json(['detailOrder' => $formattedDetailOrder,]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    private function formatOrder(Orders $orders)
+    {
+        return [
+            'id' => $orders->id,
+            'user_id' => $orders->user_id,
+            'customer_name' => $orders->customer_name,
+            'order_time' => $orders->order_time,
+            'created_at' => $orders->created_at,
+            'updated_at' => $orders->updated_at
+        ];
+    }
+}
