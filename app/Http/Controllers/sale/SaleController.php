1c36c0eba46fa9699c37f5830056d187f33cdfb9
anandsathya80 <anandsathya80@gmail.com>
2024-08-08 16:11:18 +0700

    feat: get total price from detailOrders bas on date

--- app/Http/Controllers/sale/SaleController.php
+++ app/Http/Controllers/sale/SaleController.php
@@ -7,6 +7,7 @@ use App\Models\Foods;
 use App\Models\FoodType;
 use App\Models\OrderDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SaleController extends Controller
 {
@@ -14,17 +15,21 @@ class SaleController extends Controller
         string $startDate,
         string $endDate,
     ) {
-        $totalFood = OrderDetail::groupBy('food_id')->selectRaw('sum(food_qty) as food_qty, food_id')
+        $totalFood = OrderDetail::groupBy('food_id')
+            ->selectRaw('sum(food_qty) as food_qty, food_id')
             ->whereDate('created_at', '>=', $startDate)
             ->whereDate('created_at', '<=', $endDate)->get();
         $formattedTotalFood = $totalFood->map(function ($totalFood) {
             return $this->formatCountFood($totalFood);
         });
 
+        $totalPrice = OrderDetail::join('foods', 'order_details.food_id', '=', 'foods.id')
+            ->sum(DB::raw('order_details.food_qty * foods.price'));
         return response()->json([
             'totalFood' => $formattedTotalFood,
             'from_date' => $startDate,
             'to_date' => $endDate,
+            'total_price_sales' => $totalPrice,
         ]);
     }
 
@@ -35,8 +40,10 @@ class SaleController extends Controller
         return [
             'food_type' => $foodtype ? $foodtype->food_type_name : null,
             'food_name' => $food ? $food->name : null,
+            'food_price' => $food->price,
             'qty' => $orderDetail->food_qty,
             'food_id' => $orderDetail->food_id,
+            'total_sales_per_food' => $orderDetail->food_qty * $food->price,
         ];
     }
 }
