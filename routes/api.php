55c1e1920e5669e8c6d69f244f2b73a141d6a7c3
anandsathya80 <anandsathya80@gmail.com>
2024-08-09 14:31:22 +0700

    refactor: route: with middleware

--- routes/api.php
+++ routes/api.php
@@ -26,48 +26,42 @@ use App\Http\Controllers\sale\SaleController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// food type
-Route::get('/foodType', [FoodTypeController::class, 'index']);
-Route::post('/foodType', [FoodTypeController::class, 'store']);
+Route::middleware('auth:api')->group(function () {
+    // food type
+    Route::get('/foodType', [FoodTypeController::class, 'index']);
+    Route::post('/foodType', [FoodTypeController::class, 'store']);
 
 
-// paymnet type
-Route::get('/payment', [PaymentController::class, 'index']);
-Route::post('/payment', [PaymentController::class, 'store']);
+    // paymnet type
+    Route::get('/payment', [PaymentController::class, 'index']);
+    Route::post('/payment', [PaymentController::class, 'store']);
 
-// foods
-Route::get('/foods', [FoodController::class, 'index']);
-Route::post('/foods', [FoodController::class, 'store']);
+    // foods
+    Route::get('/foods', [FoodController::class, 'index']);
+    Route::post('/foods', [FoodController::class, 'store']);
 
-// order
-Route::get('/orders', [OrderController::class, 'index']);
-Route::post('/orders', [OrderController::class, 'store']);
+    // order
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/orders', [OrderController::class, 'store']);
 
-// summary order
-Route::get('/summaryOrders/{id}', [OrderSummaryController::class, 'show']);
-Route::post('/summaryOrders', [OrderSummaryController::class, 'store']);
+    // summary order
+    Route::get('/summaryOrders/{id}', [OrderSummaryController::class, 'show']);
+    Route::post('/summaryOrders', [OrderSummaryController::class, 'store']);
 
-// POS by method_payment
-Route::get('/sumMethodPayment/{id}/{startDate}/{endDate}', [PaymentController::class, 'show']);
+    // POS by method_payment
+    Route::get('/sumMethodPayment/{id}/{startDate}/{endDate}', [PaymentController::class, 'show']);
 
-// POS by food
-Route::get('/sumFood/{startDate}/{endDate}', [SaleController::class, 'countFood']);
+    // POS by food
+    Route::get('/sumFood/{startDate}/{endDate}', [SaleController::class, 'countFood']);
 
-// POS by server
-Route::get('/sumServer/{id}/{startDate}/{endDate}', [SaleController::class, 'countSalesByServer']);
+    // POS by server
+    Route::get('/sumServer/{id}/{startDate}/{endDate}', [SaleController::class, 'countSalesByServer']);
 
-// POS by date interval
-Route::get('/sumDate/{startDate}/{endDate}', [SaleController::class, 'countByDate']);
+    // POS by date interval
+    Route::get('/sumDate/{startDate}/{endDate}', [SaleController::class, 'countByDate']);
 
 
-// order detail
-Route::get('/orderDetail', [OrderDetailController::class, 'index']);
-Route::post('/orderDetail', [OrderDetailController::class, 'store']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // order detail
+    Route::get('/orderDetail', [OrderDetailController::class, 'index']);
+    Route::post('/orderDetail', [OrderDetailController::class, 'store']);
 });
