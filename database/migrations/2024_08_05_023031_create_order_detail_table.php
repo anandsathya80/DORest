42144d6fef84cf3361b1280b3f3b359c778a50ba
anandsathya80 <anandsathya80@gmail.com>
2024-08-12 14:56:50 +0700

    fix: repair migratin order_details table

--- database/migrations/2024_08_05_023031_create_order_detail_table.php
+++ database/migrations/2024_08_05_023031_create_order_detail_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_detail', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreignUuid('food_id')->references('id')->on('foods')->onDelete('cascade');
